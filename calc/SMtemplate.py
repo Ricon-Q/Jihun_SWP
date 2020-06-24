2a103762e5e6f0580391ef1770ab83821cacb66e
Ricon-Q <ricon4246@kookmin.ac.kr>
2020-06-24 12:57:49 +0900

    issue #5 add sum_mul.py add SMtemplate.py add sum_mul.conf

---
+++ calc/SMtemplate.py
@@ -0,0 +1,20 @@
+html = b"""
+<html>
+    <head>
+        <title>
+        KMU - 20203052 Addition & Multiplication
+        </title>
+    </head>
+    <body>
+        <form action="">
+            Input Two Number : <input type="number" name="a">
+            and <input type="number" name="b">
+            <input type="submit">
+        </form>
+        <p>
+        sum : %(sum)s</br>
+        mul : %(mul)s</br>
+        </p>
+    </body>
+</html>
+"""
