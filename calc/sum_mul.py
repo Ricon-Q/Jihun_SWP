c19eb06523a9a0826bf99c341a93eaf1f2f0088a
Ricon-Q <ricon4246@kookmin.ac.kr>
2020-06-26 09:59:20 +0900

    issue #9 Except If I submit wrong letter, It will print -999 modify sum_mul.py line 11 delete add try: except ValueError: 	sum = -999 	mul = -999

--- calc/sum_mul.py
+++ calc/sum_mul.py
@@ -6,19 +6,21 @@
      a = d.get('a', [''])[0]
      b = d.get('b', [''])[0]
      sum, mul =20203052, 20203052
-     if '' not in [a, b]:
+     
+	 try:
          a = int(a)
          b = int(b)
  
          sum = a + b
          mul = a * b
- 
+	 except ValueError:
+		 sum = -999
+		 mul = -999
      response_body = html % {
          'sum' : sum, 'mul' : mul
          }
      start_response('200 OK', [
          ('Content-Type', 'text/html'),
-         ('Content-Length', str(len(response_body)))
+   	   	 ('Content-Length', str(len(response_body)))
      ])
-     return [response_body]
-
+    return [response_body]
